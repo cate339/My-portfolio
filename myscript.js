2d339a1499f3762520d33d5bbb1c19caa2025432
Cate Wanja <mangaracatherine@gmail.com>
2023-08-10 16:59:59 +0300

    Add pop-up html

--- myscript.js
+++ myscript.js
@@ -14,6 +14,7 @@ document.querySelectorAll('.navLink').forEach((n) => n
 
 // mobile cards object
 const cards = [{
+    id: 1,
     snapshot: '<img class="snapshot" src= "./images/Snapshoot Portfolio.png"/>',
     title: "Tonic",
     client: "Canopy",
@@ -28,6 +29,7 @@ const cards = [{
     action: "See project",
 },
 {
+    id: 2,
     snapshot: '<img class="snapshot" src= "./images/Snapshoot Portfolio (1).png"/>',
     title: "Multi-Post Stories",
     client: "Canopy",
@@ -42,6 +44,7 @@ const cards = [{
     action: "See project",
 },
 {
+    id: 3,
     snapshot: '<img class="snapshot" src= "./images/Snapshoot Portfolio (2).png"/>',
     title: "Tonic",
     client: "Canopy",
@@ -56,6 +59,7 @@ const cards = [{
     action: "See project",
 },
 {
+    id: 4,
     snapshot: '<img class="snapshot" src= "./images/Snapshoot Portfolio (3).png"/>',
     title: "Multi-Post Stories",
     client: "Canopy",
@@ -68,6 +72,72 @@ const cards = [{
     skills2: "CSS",
     skills3: "JAVASCRIPT",
     action: "See project",
+}];
+
+// desktop cards object
+
+const work = [{
+    id: 1,
+    snapshot: '<img class="snapshot" src="./images/Portoflio Card/Snapshoot Portfolio.png">',
+    title: "Tonic",
+    client: "CANOPY",
+    counter: '<img class="counter" src="./images/Counter.png"/>',
+    role: "Back End Dev",
+    counter: '<img class="counter" src="./images/Counter.png"/>',
+    year: "2015",
+    primary: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    skills: "html",
+    skills1: "css",
+    skills2: "javascript",
+    action: "See project",
+},
+{
+    id: 2,
+    snapshot: '<img class="snapshot" src="./images/Snapshoot Portfolio (3).png"/>',
+    title: "Multi-Post Stories",
+    client: "FACEBOOK",
+    counter: '<img class="counter" src="./images/Counter.png"/>',
+    role: "Full Stack Dev",
+    counter: '<img class="counter" src="./images/Counter.png"/>',
+    year: "2015",
+    primary: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    skills: "html",
+    skills1:"ruby on rails",
+    skills2: "css",
+    skills3: "javascript",
+    action: "See project", 
+},
+{
+    id: 3,
+    snapshot: '<img class="snapshot" src="./images/Snapshoot Portfolio.png"/>',
+    title: "Facebook 360",
+    client: "FACEBOOK",
+    counter: '<img class="counter" src="./images/Counter.png"/>',
+    role: "Full Stack Dev",
+    counter: '<img class="counter" src="./images/Counter.png"/>',
+    year: "2015",
+    primary: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    skills: "html",
+    skills1:"ruby on rails",
+    skills2: "css",
+    skills3: "javascript",
+    action: "See project", 
+},
+{
+    id: 4,
+    snapshot: '<img class="snapshot" src="./images/Snapshoot Portfolio (1).png">',
+    title: "Uber Navigation",
+    client: "Uber",
+    counter: '<img class="counter" src="./images/Counter.png"/>',
+    role: "Lead Developer",
+    counter: '<img class="counter" src="./images/Counter.png"/>',
+    year: "2018",
+    primary: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    skills: "html",
+    skills1:"ruby on rails",
+    skills2: "css",
+    skills3: "javascript",
+    action: "See project", 
 }];
 
 // mobile cards function
@@ -102,12 +172,86 @@ function addMobileCards(){
                     <li class="skills">${card.skills3}</li>
                 </ul>
             </div>
-            <button class="action">${card.action}</button>
+            <a href="#${card.id}"class="action">See project</a>
         </div>
     </div>`
     });
     mobileContainer.innerHTML = mobileCards;
 };
 
-addMobileCards();
-}
+
+// desktop cards function
+
+    const desktopContainer = document.getElementById('desktop-cards')
+    
+    function addDesktopCards(){
+        let desktopCards = '';
+    
+        work.forEach((item, index) => {
+            if(index==1 || index==3) {
+                desktopCards += `
+                <div class="card-works card-work2">
+                ${item.snapshot}
+                <div class="left-block">
+                    <div class="primary-text">
+                        <h1 class="title">${item.title}</h1>
+                        <div class="frame2">
+                            <h2 class="client">${item.client}</h2>
+                        ${item.counter}
+                            <h3 class="role">${item.role}</h3>
+                        ${item.counter}
+                            <h3 class="year">${item.year}</h3>
+                
+                        </div>
+                    </div>
+                    <p class="primary">${item.primary}</p>
+                    <div class="tags">
+                        <ul class="list">
+                            <li class="skills">${item.skills}</li>
+                            <li class="skills">${item.skills1}</li>
+                            <li class="skills">${item.skills2}</li>
+                            <li class="skills">${item.skills3}</li>
+                        </ul>
+                    </div>
+                    <a href="#${item.id}"class="action">See project</a>
+                </div>
+                </div>`
+            
+            } else{
+                desktopCards += `
+                <div class="card-works">
+                ${item.snapshot}
+                <div class="left-block">
+                    <div class="primary-text">
+                        <h1 class="title">${item.title}</h1>
+                        <div class="frame2">
+                            <h2 class="client">${item.client}</h2>
+                        ${item.counter}
+                            <h3 class="role">${item.role}</h3>
+                        ${item.counter}
+                            <h3 class="year">${item.year}</h3>
+                
+                        </div>
+                    </div>
+                    <p class="primary">${item.primary}</p>
+                    <div class="tags">
+                        <ul class="list">
+                            <li class="skills">${item.skills}</li>
+                            <li class="skills">${item.skills1}</li>
+                            <li class="skills">${item.skills2}</li>
+                            <li class="skills">${item.skills3}</li>
+                        </ul>
+                    </div>
+                    <a href="#${item.id}"class="action">See project</a>
+                </div>
+                </div>`
+            };
+            });
+
+        desktopContainer.innerHTML = desktopCards;
+    };
+
+    addMobileCards();
+    
+    addDesktopCards();
+};
